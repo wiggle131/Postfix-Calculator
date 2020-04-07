d5ebb871a8c6cc7c37709d2b22d41d0179ba1df4
Jhosie Charles Espantaleon <jhosiecharlesespantaleon@gmail.com>
2020-04-07 23:38:37 +0800

    Typo in Kindness

--- Postfix.java
+++ Postfix.java
@@ -4,7 +4,7 @@ public class Postfix {
   public static void main(String[] args) {
     Scanner a = new Scanner(System.in);
     String operation;
-    System.out.println("Pleadr Input (with spaces after each operand/operator) :");
+    System.out.println("Please Input (with spaces after each operand/operator) :");
     operation = a.nextLine();
     Post op = new Post(operation);
     op.start();
